5648ddc7553358ee6d1ee97a9b2f86f9225cc38e
Manohar Bhat <mbhatb@smartshift.com>
2024-07-05 10:33:22 +0530

    added my-attempt - 03-Function Practice Exercises.ipynb, my-attempt - 07-Statements Assessment Test.ipynb, myattempt - 07-Statements Assessment Test.ipynb
    updated python-basics.ipynb python-basics.py

--- python-basics.py
+++ python-basics.py
@@ -15,3 +15,17 @@ print({"k1":"v1",3:54,"k1":"v2"})
 
 # tuple
 print(("string value",5,3,4,1))
+
+# pyright gives error as type annotation is declared for str but int value is assigned
+# code executes fine though
+programming_language: str = 'Python'
+programming_language=1
+print(programming_language)
+
+# lambda functions in python
+def square(num):
+    return num**2
+
+my_nums = [1,2,3,4,5]
+
+print(map(square,my_nums))
